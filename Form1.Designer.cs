b01f8e6d426497712dae12294f2cab045fe9fc97
Валерия Воликова <lerusic2002@rambler.ru>
2021-12-16 20:52:53 +0800

    Создание частиц исходящих из центра (по методичке)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,9 @@ namespace Kyrsach
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,6 +42,11 @@ namespace Kyrsach
             this.picDisplay.Size = new System.Drawing.Size(776, 426);
             this.picDisplay.TabIndex = 0;
             this.picDisplay.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -57,6 +64,7 @@ namespace Kyrsach
         #endregion
 
         private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Particle.cs
+++ Particle.cs
@@ -3,13 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing; // чтобы исплоьзовать Graphics
 
 namespace Kyrsach
 {
-    class Particle
+    public class Particle
     {
         public int Radius; // радуис частицы
         public float X; // X координата положения частицы в пространстве
         public float Y; // Y координата положения частицы в пространстве
+        public float Direction; // направление движения угол
+        public float Speed; // скорость перемещения
+        public float Life; // запас здоровья частицы
+                           // добавили генератор случайных чисел
+        public static Random rand = new Random();
+
+        // конструктор по умолчанию будет создавать кастомную частицу
+        public Particle()
+        {
+            // я не трогаю координаты X, Y потому что хочу, чтобы все частицы возникали из одного места
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 4 + rand.Next(10);
+            Life = 20 + rand.Next(100);
+        }
+        public virtual void Draw(Graphics g)
+        {
+            // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
+            float k = Math.Min(1f, Life / 100);
+            // рассчитываем значение альфа канала в шкале от 0 до 255
+            // по аналогии с RGB, он используется для задания прозрачности
+            int alpha = (int)(k * 255);
+
+            // создаем цвет из уже существующего, но привязываем к нему еще и значение альфа канала
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
+            // нарисовали залитый кружок радиусом Radius с центром в X, Y
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+            b.Dispose();
+        }
+
+    }
+    // новый класс для цветных частиц
+    public class ParticleColorful : Particle
+    {
+        // два новых поля под цвет начальный и конечный
+        public Color FromColor;
+        public Color ToColor;
+
+        // для смеси цветов
+        public static Color MixColor(Color color1, Color color2, float k)
+        {
+            return Color.FromArgb(
+                (int)(color2.A * k + color1.A * (1 - k)),
+                (int)(color2.R * k + color1.R * (1 - k)),
+                (int)(color2.G * k + color1.G * (1 - k)),
+                (int)(color2.B * k + color1.B * (1 - k))
+            );
+        }
+
+        // ну и отрисовку перепишем
+        public override void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);
+
+            // так как k уменьшается от 1 до 0, то порядок цветов обратный
+            var color = MixColor(ToColor, FromColor, k);
+            var b = new SolidBrush(color);
+
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
+        }
     }
 }
